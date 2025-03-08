4bfd4ca04d17361122b1a43d1bbfabd3a79d8a29
KlatosThGOS-024 <priyanshutiwari878@yahoo.com>
2025-03-08 19:34:39 +0530

    First commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+let is_male:bool = false;
+let is_adult : bool = false;
+if is_male&&is_adult {
+    print!("He is male and adult")
+}
+else if !is_male&&is_adult{
+    print!("He is not male but adult")
+
+}else{
+    print!("He is not male and adult")
+}
+}
